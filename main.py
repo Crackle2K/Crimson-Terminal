7368b40f2a6e3b462e72c2d332f2a81a432f1362
Dinesh <crackleontwitch@gmail.com>
2025-07-25 09:12:42 -0400

    Adding Custom GUI

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from sequences.intro import intro_sequence
 from sequences.battle import battle_sequence
+import tkinter as tk
 
-if __name__ == "__main__":
+window = tk.Tk()
+window.title("Fullscreen Window")
+window.geometry("854x480")
+
+label = tk.Label(window, text="Hello, World!", font=("Arial", 16))
+label.pack(pady=20)
+
+window.mainloop()
+
+
+# if __name__ == "__main__":
     # intro_sequence()
-    battle_sequence()
+    # battle_sequence()
